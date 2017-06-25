95d79fb96bd8f43364e9c17ef7cd9d97dac945d7
Liam Mitchell <liamkarlmitchell@gmail.com>
2017-06-25 12:21:26 +1200

    Update

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ module.exports = function(options) {
 
     // Note: Using dependency graph because each module has its own require cache but we want a shared one.
     if (!graph.hasNode(resolvedModulePath)) {
-      emitter.emit('loaded', modulePath);
+      emitter.emit('loaded', modulePath, requiredModule);
     }
 
     graph.addNode(this.filename);
@@ -150,17 +150,19 @@ module.exports = function(options) {
       copyLeft(target.prototype, source.prototype);
     }
 
+    // Purge old keys that no longer exist first.
+    for (var key in target) {
+      if (source[key] === undefined) {
+        delete target[key];
+      }
+    }
+    // Copy over the new keys.
     for (var key in source) {
       target[key] = source[key];
 
       // Handle functions with prototypes exported.
       if (target[key].prototype && source[key].prototype) {
         copyLeft(target[key].prototype, source[key].prototype);
-      }
-    }
-    for (var key in target) {
-      if (source[key] === undefined) {
-        delete target[key];
       }
     }
   }
@@ -190,8 +192,8 @@ module.exports = function(options) {
           copyLeft(oldExports, newExports);
         }
 
-        emitter.emit('loaded', modulePath);
-        emitter.emit('reloaded', modulePath);
+        emitter.emit('loaded', modulePath, newExports);
+        emitter.emit('reloaded', modulePath, newExports);
       }
     } catch (e) {
       // Restore the old cache?
